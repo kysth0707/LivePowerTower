7eb924919c7c0e815fd2d6943c7bcf781f91ddbb
Kim TaeHyeong <kysth0707@naver.com>
2024-07-05 12:21:55 +0900

    더 디테일한 세부사항 업데이트

--- live.py
+++ live.py
@@ -1,5 +1,5 @@
 # 화소 및 데이터 변경
-max_iter = 500
+maxIter = 512
 escape_radius = 1e+10
 
 # (x, y, eps) = (-4.133439593177311, 0.00011276978589557028, 1.0351947055640964e-11)
@@ -58,6 +58,8 @@ mapY = y
 # 시작 좌우 길이
 mapShowOffset = eps
 
+renderBlocks = [True for _ in range(chunk)]
+
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 YELLOW = (255, 255, 0)
@@ -72,8 +74,11 @@ isDrawn, oneChunckLen, mapImage = getResetedDraw(MAP_RENDER_SIZE, mapY, mapShowO
 
 # 그리기 변수
 boxStartPos = (0, 0)
+partRenderStartPos = (0, 0)
 boxDragging = False
 moveDragging = False
+partRenderDragging = False
+isShift = False
 
 run = True
 while run:
@@ -84,8 +89,12 @@ while run:
 			if not value:
 				drawAll = False
 				y,mapdrawy=key
+
 				length = int(MAP_RENDER_SIZE/chunk)
-				tmp = compute_tetration_divergence(mapX - mapShowOffset, y, mapX + mapShowOffset, y+oneChunckLen, MAP_RENDER_SIZE, length)
+				if renderBlocks[mapdrawy] == True: # 해당 위치를 렌더한다면
+					tmp = compute_tetration_divergence(mapX - mapShowOffset, y, mapX + mapShowOffset, y+oneChunckLen, MAP_RENDER_SIZE, length, maxIter)
+				else:
+					tmp = np.zeros((MAP_RENDER_SIZE, length), dtype=bool)
 				mapImage[:,mapdrawy*length:(mapdrawy+1)*length] = tmp
 
 				isDrawn['map'][key] = True
@@ -93,9 +102,9 @@ while run:
 			
 		if drawAll:
 			isDrawn['drawAll'] = True
+			renderBlocks = [True for _ in range(chunk)]
 			print(f"(x, y, eps) = ({mapX}, {mapY}, {mapShowOffset})")
-		
-
+	
 	# 화면 그리기
 	screen.fill((0, 0, 0))
 
@@ -116,8 +125,8 @@ while run:
 
 	# 프레임 그리기
 	screen.blit(myFont.render(f"{int(1/(time.time() - lastTime))} fps", True, (0, 0, 255)), (0, 0))
-	screen.blit(myFont.render(f"왼쪽 드래그 : 확대 / 휠 드래그 : 이동 / 우클릭 : 2배 축소 / ↑ : 화질 2배 / ↓ : 화질 0.5배", True, (0, 0, 255)), (100, 0))
-	screen.blit(myFont.render(f"x {mapX} / y {mapY} / eps {mapShowOffset} / Image {MAP_RENDER_SIZE} X {MAP_RENDER_SIZE}", True, (0, 0, 255)), (0, SCREEN_HEIGHT - DRAW_OFFSET))
+	screen.blit(myFont.render(f"왼쪽 드래그:확대/휠 드래그:이동/Z:2배 축소/↑:화질 2배/↓:0.5배/←:반복 0.5배/→:2배/Shift←→:+-1", True, (0, 0, 255)), (100, 0))
+	screen.blit(myFont.render(f"x {mapX} / y {mapY} / eps {mapShowOffset} / Image {MAP_RENDER_SIZE} X {MAP_RENDER_SIZE} / maxIter {maxIter}", True, (0, 0, 255)), (0, SCREEN_HEIGHT - DRAW_OFFSET))
 	lastTime = time.time()
 
 	# 드래그 상자 그리기
@@ -128,6 +137,16 @@ while run:
 			pygame.draw.rect(screen, YELLOW, [boxStartPos[0], boxStartPos[1], size, size], 2)
 		else:
 			pygame.draw.rect(screen, YELLOW, [boxStartPos[0]+size, boxStartPos[1]+size, -size, -size], 2)
+	
+	if partRenderDragging:
+		mPos = pygame.mouse.get_pos()
+		startX = int((partRenderStartPos[0] - DRAW_OFFSET) / ((SCREEN_WIDTH - 2*DRAW_OFFSET) / chunk))
+		endX = int((mPos[0] - DRAW_OFFSET) / ((SCREEN_WIDTH - 2*DRAW_OFFSET) / chunk))
+		if endX > 15:
+			endX = 15
+
+		pygame.draw.rect(screen, YELLOW, [int(startX * (SCREEN_WIDTH - 2*DRAW_OFFSET) / chunk) + DRAW_OFFSET, DRAW_OFFSET, int((endX - startX + 1) * (SCREEN_WIDTH - 2*DRAW_OFFSET) / chunk), SCREEN_HEIGHT - 2*DRAW_OFFSET], 5)
+
 
 	pygame.display.update()
 
@@ -141,11 +160,44 @@ while run:
 		elif event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_UP:
 				MAP_RENDER_SIZE *= 2
-				isDrawn, oneChunckLen, mapImage = getResetedDraw(MAP_RENDER_SIZE, mapY, mapShowOffset, chunk)
+
 			elif event.key == pygame.K_DOWN:
 				if MAP_RENDER_SIZE > 2:
 					MAP_RENDER_SIZE = int(MAP_RENDER_SIZE/2)
+
+			elif event.key == pygame.K_UP:
+				MAP_RENDER_SIZE *= 2
+
+			elif event.key == pygame.K_DOWN:
+				if MAP_RENDER_SIZE >= 2:
+					MAP_RENDER_SIZE = int(MAP_RENDER_SIZE/2)
+
+			elif event.key == pygame.K_RIGHT:
+				if isShift:
+					maxIter += 1
+				else:
+					maxIter *= 2
+
+			elif event.key == pygame.K_LEFT:
+				if maxIter >= 2:
+					if isShift:
+						maxIter -= 1
+					else:
+						maxIter = int(maxIter/2)
+
+			elif event.key == pygame.K_z:
+				if not boxDragging and not moveDragging:
+					mapShowOffset = mapShowOffset*2
+			
+			elif event.key == pygame.K_LSHIFT:
+				isShift = True
+			
+			if event.key in [pygame.K_UP, pygame.K_DOWN, pygame.K_UP, pygame.K_RIGHT, pygame.K_LEFT, pygame.K_z]:
 					isDrawn, oneChunckLen, mapImage = getResetedDraw(MAP_RENDER_SIZE, mapY, mapShowOffset, chunk)
+		
+		elif event.type == pygame.KEYUP:
+			if event.key == pygame.K_LSHIFT:
+				isShift = False
 
 		elif event.type == pygame.MOUSEBUTTONDOWN:
 			if event.button == 1: #좌클
@@ -155,7 +207,9 @@ while run:
 				boxStartPos = pygame.mouse.get_pos()
 				moveDragging = True
 			elif event.button == 3:
-				pass
+				if not boxDragging:
+					partRenderStartPos = pygame.mouse.get_pos()
+					partRenderDragging = True
 		elif event.type == pygame.MOUSEBUTTONUP:
 			if event.button == 1: #좌클
 				if boxDragging:
@@ -195,10 +249,20 @@ while run:
 					isDrawn, oneChunckLen, mapImage = getResetedDraw(MAP_RENDER_SIZE, mapY, mapShowOffset, chunk)
 
 			elif event.button == 3:
-				if not boxDragging and not moveDragging:
-					mapShowOffset = mapShowOffset*2
+				if partRenderDragging:
+					partRenderDragging = False
 					
+					mPos = pygame.mouse.get_pos()
+					startX = int((partRenderStartPos[0] - DRAW_OFFSET) / ((SCREEN_WIDTH - 2*DRAW_OFFSET) / chunk))
+					endX = int((mPos[0] - DRAW_OFFSET) / ((SCREEN_WIDTH - 2*DRAW_OFFSET) / chunk))
+					if endX > 15:
+						endX = 15
+
+					renderBlocks = [
+						(startX <= i and i <= endX) for i in range(chunk)
+					]
 					isDrawn, oneChunckLen, mapImage = getResetedDraw(MAP_RENDER_SIZE, mapY, mapShowOffset, chunk)
+					# print(renderBlocks)
 
 			if event.button != 1:
 				boxDragging = False
